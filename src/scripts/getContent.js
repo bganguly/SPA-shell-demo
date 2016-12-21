aebde8aef58f801c1970bfe44bb65a168a3694c9
bganguly <gangulybikramjit@hotmail.com>
2016-12-20 22:47:32 -0800

    fix styling and refactor js

--- src/scripts/getContent.js
+++ src/scripts/getContent.js
@@ -1,6 +1,3 @@
-/**
- *
- */
 "use strict";
 
 /**
@@ -30,19 +27,38 @@ function getPromise(url, responseTypeExpected) {
 /**
  * make the xhr call and process response
  */
-var createObjFromResponse = function() {
+var renderDomFromResponse = function() {
     getPromise('../data.json','json').then(function(response) {
-        console.log(JSON.parse(response));
         renderJsonContent(JSON.parse(response));
     }, function(msg) {
         console.error(msg);
     });
 };
+
 /**
- * populate divs with json response callback
+ * returns sorted object by string keys
+ * @param objAttribute
  */
-var renderJsonContent = function(responseArray) {
-    var contentEl = document.getElementsByClassName('content')[0];
+var sortObjByAttribute = function(objAttribute){
+    return function(objA,objB) {
+        var aAttribute = objA[objAttribute].toUpperCase();
+        var bAttribute = objB[objAttribute].toUpperCase();
+        if (aAttribute < bAttribute) {
+            return -1;
+        } else if (aAttribute > bAttribute) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+};
+
+/**
+ * Generate markup for each div
+ * @param eachObjInPublishedArray
+ * @returns {Element|*}
+ */
+var generateEachDiv = function (eachObjInPublishedArray) {
     var divNode;
     var textNode;
     var imgNode;
@@ -50,43 +66,67 @@ var renderJsonContent = function(responseArray) {
     var imgFileNode;
     var lineSeparatorNode;
     var divWithDescriptionNode;
-    var eachObjInResponse;
+
+    divNode = document.createElement('div');
+
+    imgNode = document.createElement('img');
+    imgNode['src'] = "images/" + eachObjInPublishedArray['image_name'];
+    divNode.appendChild(imgNode);
+
+    imgTitleNode = document.createElement('div');
+    imgTitleNode.className = "img-title";
+    textNode = document.createTextNode(eachObjInPublishedArray['title']);
+    imgTitleNode.appendChild(textNode);
+    divNode.appendChild(imgTitleNode);
+
+    imgFileNode = document.createElement('div');
+    imgFileNode.className = "img-file";
+    textNode = document.createTextNode(eachObjInPublishedArray['image_name']);
+    imgFileNode.appendChild(textNode);
+    divNode.appendChild(imgFileNode);
+
+    divWithDescriptionNode = document.createElement('div');
+    divWithDescriptionNode.className = "img-description";
+    textNode = document.createTextNode(eachObjInPublishedArray['description']);
+    divWithDescriptionNode.appendChild(textNode);
+    divNode.appendChild(divWithDescriptionNode);
+
+    return divNode;
+};
+
+/**
+ *
+ * @param publishedArray
+ * @param contentEl
+ */
+var updateContentDiv = function (publishedArray,contentEl) {
+    var newlyCreatedDivs = publishedArray.map(generateEachDiv);
+    newlyCreatedDivs.forEach(function(eachNewlyCreatedDiv){
+        contentEl.appendChild(eachNewlyCreatedDiv);
+    });
+};
+
+/**
+ * populate divs with json response callback
+ */
+var renderJsonContent = function(responseArray) {
+    var contentEl = document.getElementsByClassName('content')[0];
+    var publishedArray;
+
     // flush content in preparation for renedering new json data
     contentEl.innerHTML = "";
-    for (var idx=0; idx<responseArray.length;idx++) {
-        eachObjInResponse = responseArray[idx];
-
-        divNode= document.createElement('div');
-        divNode.className = "align-center";
-
-        imgNode= document.createElement('img');
-        imgNode['src'] = "images/"+eachObjInResponse['image_name'];
-        divNode.appendChild(imgNode);
-
-        imgTitleNode= document.createElement('div');
-        imgTitleNode.className = "img-title";
-        textNode = document.createTextNode(eachObjInResponse['title']);
-        imgTitleNode.appendChild(textNode);
-        divNode.appendChild(imgTitleNode);
-
-        imgFileNode= document.createElement('div');
-        imgFileNode.className = "img-file";
-        textNode = document.createTextNode(eachObjInResponse['image_name']);
-        imgFileNode.appendChild(textNode);
-        divNode.appendChild(imgFileNode);
-
-        divWithDescriptionNode= document.createElement('div');
-        divWithDescriptionNode.className = "img-description";
-        textNode = document.createTextNode(eachObjInResponse['description']);
-        divWithDescriptionNode.appendChild(textNode);
-        divNode.appendChild(divWithDescriptionNode);
-
-        contentEl.appendChild(divNode);
-    }
-
-}
+    // create temparray for entries where is_published === true
+    publishedArray = responseArray.filter(function (eachObjInResponse) {
+        return eachObjInResponse["is_published"] === true;
+    });
+    // sort publishedArray by img title (default behavior on pg load)
+    // "A" sorts before "B"
+    publishedArray = publishedArray.sort(sortObjByAttribute("title"));
+    // update DOM to insert nodes using content from  publishedArray
+    updateContentDiv(publishedArray,contentEl);
+};
 
 /**
  * call the main function
  */
-createObjFromResponse();
+renderDomFromResponse();
